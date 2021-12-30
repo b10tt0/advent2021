d4bfb78eb2827286050549b23cc6e7719d7a2f05
b10tt0 <b4b0000000@gmail.com>
2021-12-30 23:58:42 +0000

    day-5 done

--- day-5/day-5.py
+++ day-5/day-5.py
@@ -3,6 +3,7 @@ import re
 data = open('input.txt', 'r', encoding='utf-8').read()
 just_data = [int(i) for i in re.split(',|->| |\n', data) if i != '']
 graph = [['.']*(max(just_data)+1) for i in range(max(just_data)+1)]
+plus_5 = [['.']*(max(just_data)+1) for i in range(max(just_data)+1)]
 
 print(just_data)
 
@@ -17,36 +18,54 @@ def print_graph():
 def get_coord(coord):
     return graph[coord[0]][coord[1]]
 
+def add_to_plus_5(x, y):
+    if graph[x][y] >= 2 and plus_5[x][y] == '.':
+        plus_5[x][y] = 'X'
+
+def count_plus_5(): 
+    count = 0
+    for x in plus_5:
+        for i in x:
+            if i == 'X':
+                count += 1
+    return count
+
 def add_to_graph(s, e):
-    diff = (e[0]-s[0], e[1]-s[1])
-    if diff[0] == 0: # if y changes
-        if diff[1] > 0:
-            r = (s[1], e[1]+1)
+    diff = (e[0]-s[0], e[1]-s[1]) # assume a change
+    print(diff)
+    x_inc=y_inc=0
+    if e[0] > s[0]: # if x increases
+        x_inc = 1
+    if e[0] < s[0]: # if x decreases
+        x_inc = -1
+    if e[0] == s[0]: # if x stays the same
+        x_inc = 0
+
+    if e[1] > s[1]:
+        y_inc = 1
+    if e[1] < s[1]:
+        y_inc = -1
+    if e[1] == s[1]:
+        y_inc = 0
+   
+    r = max((abs(diff[0]), abs(diff[1]))) # range
+    x, y = s[0], s[1]
+    for i in range(r+1):
+        if graph[y][x] == '.':
+            graph[y][x] = 1
         else:
-            r = (e[1], s[1]+1)
-        for y in range(r[0], r[1]):
-            if graph[y][s[0]] == '.':
-                graph[y][s[0]] = 1
-            else:
-                graph[y][s[0]] += 1
-            
-    else: # if x change
-        if diff[0] > 0:
-            r = (s[0], e[0]+1)
-        else:
-            r = (e[0], s[0]+1)
-        for x in range(r[0], r[1]):
-            if graph[s[1]][x] == '.':
-                graph[s[1]][x] = 1
-            else:
-                graph[s[1]][x] += 1
+            graph[y][x] += 1
+            add_to_plus_5(y, x)
+        x += x_inc
+        y += y_inc
+
 
 i = 0
 while i < len(just_data):
     s, e = (just_data[i],just_data[i+1]), (just_data[i+2],just_data[i+3])
-    if s[0] == e[0] or s[1] == e[1]: 
-        print(str(s) + '->' + str(e))
-        add_to_graph(s, e)
+    print(str(s) + '->' + str(e))
+    add_to_graph(s, e)
     i+=4
 
-print_graph() 
+print_graph()
+print(count_plus_5())
